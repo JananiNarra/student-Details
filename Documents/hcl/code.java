425f493b5748b2fd9a3f4d568f1b269bc7e66170
JananiNarra <narrajanani@gmail.com>
2026-03-09 09:54:51 +0530

    Added student details program

---
+++ Documents/hcl/code.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Student {
+    int id;
+    String name;
+    int age;
+    String course;
+
+    void getDetails() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student ID: ");
+        id = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        System.out.print("Enter Student Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter Student Age: ");
+        age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Course: ");
+        course = sc.nextLine();
+    }
+
+    void displayDetails() {
+        System.out.println("\nStudent Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        s.getDetails();
+        s.displayDetails();
+    }
+}
